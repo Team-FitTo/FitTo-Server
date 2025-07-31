3e120220db3827171de94e29a2fc4b5484512b91
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-07-31 14:27:04 +0900

    refactor: ErrorStatus 가독성을 위한 리팩토링

--- src/main/java/com/example/fittoserver/domain/auth/service/ReissueService.java
+++ src/main/java/com/example/fittoserver/domain/auth/service/ReissueService.java
@@ -25,7 +25,7 @@ public class ReissueService {
 
         String refresh = refreshTokenService.extractRefreshToken(request);
         if (refresh == null) {
-            throw new GeneralException(ErrorStatus.INVALID_REFRESH_TOKEN); // Refresh Token이 없는 경우 예외 처리
+            throw new GeneralException(ErrorStatus.REFRESH_TOKEN_NOT_FOUND); // Refresh Token이 없는 경우 예외 처리
         }
 
         // 유효성 검사 및 예외 발생
